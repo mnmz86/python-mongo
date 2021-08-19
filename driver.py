63279524b7d85cc20cb2cefa1cff10fe86f79134
mnmz86 <martin_eros@msn.com>
2021-08-19 17:02:30 -0600

    Gestion de errores

--- driver.py
+++ driver.py
@@ -1,88 +1,90 @@
-import asyncio
-from pymongo import MongoClient
+from _typeshed import NoneType
+from typing import Union
 from config import *
+from functools import wraps # Preserva la información de funciones, como el nombre, lo uso en gestión de errores
+from pymongo import MongoClient
+from pymongo.errors import (
+    ConnectionFailure, 
+    AutoReconnect, 
+    ExecutionTimeout, 
+    ServerSelectionTimeoutError, 
+    OperationFailure)
+
+def transaccion(funcion: object) -> object:
+    @wraps(funcion)
+    def decorador(*args: Union[int, str, dict], **kwargs: int) -> object:
+        try:
+            if funcion.__name__ == "obtener_por_codigo":
+                assert type(args[1])==int, causas_fallo[no_numero]
+                assert args[1] > 9999, causas_fallo[numero_invalido]
+            if function.__name__ == ""
+            resultado = funcion(*args, *kwargs)
+            return resultado
+        except AssertionError as error:
+            return {
+                **resultados_fallidos,
+                mensaje: error.args[0]
+            }
+        except (AutoReconnect, ConnectionFailure) as errores:
+            return {
+                **resultados_fallidos, 
+                respuesta: causas_fallo[conexion_fallida] + "\n * ".join(errores.args)
+            }
+        except (ExecutionTimeout, ServerSelectionTimeoutError) as errores:
+            return {
+                **resultados_fallidos, 
+                respuesta: causas_fallo[tiempo_excedido] + "\n * ".join(errores.args)
+            }
+        except OperationFailure as errores:
+            return {
+                **resultados_fallidos,
+                respuesta: causas_fallo[peticiones_incorrectas]  + "\n * ".join(errores.args)
+            }
+        except Exception as errores:
+            return {
+                **resultados_fallidos,
+                respuesta: causas_fallo[otras_causas]  + "\n * ".join(errores.args)
+            }
+    return decorador
 
 
 class CentroEducativo():
 
     '''Clase controladora del flujo de datos hacia MongoDB'''
 
-    async def __init__(self) -> None:
-        self.estado_conexion = await self.__conexion_servidor_mongodb()
-    
-    async def __conexion_servidor_mongodb(self): 
-        try:
-            cliente = await MongoClient(mongo_uri)
-            db = await cliente["Evaluacion01"]
-            self.__coleccion_ce = await db["CE"]
-            return estado_conexion_exitoso
-        except Exception as error:
-            return {
-                **estado_conexion_fallido, 
-                respuesta:estado_conexion_fallido[respuesta] + error
-            }
+    def __init__(self) -> None:
+        self.estado_conexion = self.__conexion_servidor_mongodb()
+        
+    @transaccion
+    def __conexion_servidor_mongodb(self): 
+        cliente = MongoClient(open(".serverkey").read())
+        db = cliente["Evaluacion01"]
+        self.__coleccion_ce = db["CE"]
+        return estado_conexion_exitoso
 
-    async def agregar_centro_educativo(self, objeto_centro: object) -> object:
+    @transaccion
+    def agregar_centro_educativo(self, objeto_centro: dict) -> dict:
         pass
         #self.__coleccion_ce.insert_one(objeto_centro)
 
-    async def obtener_por_codigo(self, codigo_infraestructura: int) -> object:
-        centro_educativo = None
-        try:
-            assert type(codigo_infraestructura)==int,  "El código de infraestructura debe ser un número entero"
-            assert codigo_infraestructura > 9999, "El número ingresado no es válido"
-            centro_educativo = await self.__coleccion_ce.find_one({"CODIGO": codigo_infraestructura},{"_id":0})
-            return {
-                **devolucion_datos_exitosa,
-                respuesta:[centro_educativo]
-            }
-        except AssertionError as error:
-            return {
-                **error_devolucion_datos,
-                mensaje: error
-            }
-        except Exception as error:
-            return {
-                **estado_conexion_fallido, 
-                respuesta:estado_conexion_fallido[respuesta] + error
-            }
+    @transaccion
+    def obtener_por_codigo(self, codigo_infraestructura: int) -> object:
+        centro_educativo = self.__coleccion_ce.find_one({"CODIGO": codigo_infraestructura},{"_id":0})
+        return {
+            **devolucion_informacion_exitosa,
+            respuesta:[centro_educativo]
+        }
 
-    async def obtener_por_nombre(self, nombre_centro_educativo:str) -> object:
-        try:
-            cursor = await self.__coleccion_ce.find({"NOMBRE": {"$regex":nombre_centro_educativo}},{"_id":0})
-            coincidencias = await [centro_educativo for centro_educativo in cursor]
-            return coincidencias
-        except Exception as error:
-            return {
-                **estado_conexion_fallido, 
-                respuesta:estado_conexion_fallido[respuesta] + error
-            }
+    @transaccion
+    def obtener_por_nombre(self, nombre_centro_educativo: str) -> object:
+        cursor = self.__coleccion_ce.find({"NOMBRE": {"$regex":nombre_centro_educativo}},{"_id":0})
+        coincidencias = [centro_educativo for centro_educativo in cursor]
+        return coincidencias
 
-    async def obtener_por_departamento(self, codigo_departamento: int) -> list[object]:
-        cursor = None
-        try:
-            assert type(codigo_departamento)==int,  "El código de departamento debe ser un número entero"
-            assert codigo_departamento in  range(1,15), "El codigo de departamento ingresado estar desde 1 hasta 14"
-            cursor = await self.__coleccion_ce.find({"DEPARTAMENTO": codigo_departamento},{"_id":0})
-            return {
-                **devolucion_generador_exitosa,
-                respuesta:[cursor]
-            }
-        except AssertionError as error:
-            return {
-                **error_devolucion_datos,
-                mensaje: error
-            }
-        except Exception as error:
-            return {
-                **estado_conexion_fallido, 
-                respuesta:estado_conexion_fallido[respuesta] + error
-            }
-
-
-    async def obtener_todos(self, cantidad_a_obtener: int, cantidad_por_pagina: int = 10) -> list[object]:
+    @transaccion
+    def obtener_todos(self, cantidad_a_obtener: int, cantidad_por_pagina: int = 10) -> list[object]:
         cursor = self.__coleccion_ce.find({},{"_id": 0}).batch_size(cantidad_por_pagina)
-        async def funcion_generador():
+        def funcion_generador():
             iteracion = 0
             nonlocal cantidad_a_obtener, cantidad_por_pagina, cursor
             if not cantidad_a_obtener:
@@ -95,10 +97,8 @@ class CentroEducativo():
                 cursor.close()
         return funcion_generador
 
-    async def actualizar_centro(self, objeto_centro: object) -> object:
-        pass
-
-    async def __obtener_departamento(self, codigo_departamento: int) -> str:
+    @transaccion
+    def actualizar_centro(self, objeto_centro: object) -> object:
         pass
 
 if __name__ == "__main__":
