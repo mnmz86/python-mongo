74d9abe547f3a994dd911ecc52fea802c3cb7a08
mnmz86 <martin_eros@msn.com>
2021-08-23 00:20:27 -0600

    Hasta aquí para la tarea, lo demás por aprendizaje

--- configuracion.py
+++ configuracion.py
@@ -1,6 +1,6 @@
 from  collections import OrderedDict
-import collections
-from typing import Callable, MutableMapping
+from typing import Callable, Generator, Union, Iterator
+from warnings import catch_warnings
 from pymongo.errors import (
     ConnectionFailure,
     AutoReconnect,
@@ -9,25 +9,24 @@ from pymongo.errors import (
     OperationFailure
     )
 
-class AtributosCentroEscolar:
+class Base_:
+    @classmethod
+    def obtener_como_diccionario(cls): 
+        return cls.__annotations__
+
+
+class AtributosCentroEscolar(Base_):
     '''
     Elementos que se almacenan de un centro educativo.
     '''
 
-    CODIGO = "CODIGO"
-    NOMBRE = "NOMBRE"
-    DEPARTAMENTO_CENTRO = "DEPARTAMENTO_CENTRO"
-    MUNICIPIO_CENTRO = "MUNICIPIO_CENTRO"
+    CODIGO: int = "CODIGO" 
+    NOMBRE: str = "NOMBRE"
+    DEPARTAMENTO_CENTRO: str = "DEPARTAMENTO_CENTRO"
+    MUNICIPIO_CENTRO: str = "MUNICIPIO_CENTRO"
+    
 
-    obtener_como_objeto: Callable[[], dict] = lambda: {
-        AtributosCentroEscolar.CODIGO: 0,
-        AtributosCentroEscolar.NOMBRE: "",
-        AtributosCentroEscolar.DEPARTAMENTO_CENTRO: "",
-        AtributosCentroEscolar.MUNICIPIO_CENTRO: ""
-    }
-
-
-class UbicacionCentroEscolar():
+class UbicacionCentroEscolar(AtributosCentroEscolar):
     '''
     Códigos de departamentos y municipios.
     '''
@@ -36,9 +35,11 @@ class UbicacionCentroEscolar():
     LISTA_MUNICIPIOS = {101: 'AHUACHAPÁN', 102: 'APANECA', 103: 'ATIQUIZAYA', 104: 'CONCEPCIÓN DE ATACO', 105: 'EL REFUGIO', 106: 'GUAYMANGO', 107: 'JUJUTLA', 108: 'SAN FRANCISCO MENÉNDEZ', 109: 'SAN LORENZO', 110: 'SAN PEDRO PUXTLA', 111: 'TACUBA', 112: 'TURÍN', 201: 'CANDELARIA DE LA FRONTERA', 202: 'COATEPEQUE', 203: 'CHALCHUAPA', 204: 'EL CONGO', 205: 'EL PORVENIR', 206: 'MASAHUAT', 207: 'METAPAN', 208: 'SAN ANTONIO PAJONAL', 209: 'SAN SEBASTIÁN SALITRILLO', 210: 'SANTA ANA', 211: 'SANTA ROSA GUACHIPILÍN', 212: 'SANTIAGO DE LA FRONTERA', 213: 'TEXISTEPEQUE', 301: 'ACAJUTLA', 302: 'ARMENIA', 303: 'CALUCO', 304: 'CUISNAHUAT', 305: 'SANTA ISABEL ISHUATÁN', 306: 'IZALCO', 307: 'JUAYUA', 308: 'NAHUIZALCO', 309: 'NAHULINGO', 310: 'SALCOATITÁN', 311: 'SAN ANTONIO DEL MONTE', 312: 'SAN JULIÁN', 313: 'SANTA CATARINA MASAHUAT', 314: 'SANTO DOMINGO DE GUZMÁN', 315: 'SONSONATE', 316: 'SONZACATE', 401: 'AGUA CALIENTE', 402: 'ARCATAO', 403: 'AZACUALPA', 404: 'CITALÁ', 405: 'COMALAPA', 406: 'CONCEPCIÓN QUEZALTEPEQUE', 407: 'CHALATENANGO', 408: 'DULCE NOMBRE DE MARÍA', 409: 'EL CARRIZAL', 410: 'EL PARAISO', 411: 'LA LAGUNA', 412: 'LA PALMA', 413: 'LA REINA', 414: 'LAS VUELTAS', 415: 'NOMBRE DE JESÚS', 416: 'NUEVA CONCEPCIÓN', 417: 'NUEVA TRINIDAD', 418: 'OJOS DE AGUA', 419: 'POTONICO', 420: 'SAN ANTONIO DE LA CRUZ', 421: 'SAN ANTONIO LOS RANCHOS', 422: 'SAN FERNANDO', 423: 'SAN FRANCISCO LEMPA', 424: 'SAN FRANCISCO MORAZÁN', 425: 'SAN IGNACIO', 426: 'SAN ISIDRO LABRADOR', 427: 'CANCASQUE', 428: 'LAS FLORES', 429: 'SAN LUIS DEL CARMEN', 430: 'SAN MIGUEL DE MERCEDES', 431: 'SAN RAFAEL', 432: 'SANTA RITA', 433: 'TEJUTLA', 501: 'ANTIGUO CUSCATLÁN', 502: 'CIUDAD ARCE', 503: 'COLÓN', 504: 'COMASAGUA', 505: 'CHILTIUPÁN', 506: 'HUIZUCAR', 507: 'JAYAQUE', 508: 'JICALAPA', 509: 'LA LIBERTAD', 510: 'NUEVO CUSCATLÁN', 511: 'SANTA TECLA', 512: 'QUEZALTEPEQUE', 513: 'SACACOYO', 514: 'SAN JOSÉ VILLANUEVA', 515: 'SAN JUAN OPICO', 516: 'SAN MATÍAS', 517: 'SAN PABLO TACACHICO', 518: 'TAMANIQUE', 519: 'TALNIQUE', 520: 'TEOTEPEQUE', 521: 'TEPECOYO', 522: 'ZARAGOZA', 601: 'AGUILARES', 602: 'APOPA', 603: 'AYUTUXTEPEQUE', 604: 'CUSCATANCINGO', 605: 'EL PAISNAL', 606: 'GUAZAPA', 607: 'ILOPANGO', 608: 'MEJICANOS', 609: 'NEJAPA', 610: 'PANCHIMALCO', 611: 'ROSARIO DE MORA', 612: 'SAN MARCOS', 613: 'SAN MARTÍN', 614: 'SAN SALVADOR', 615: 'SANTIAGO TEXACUANGOS', 616: 'SANTO TOMAS', 617: 'SOYAPANGO', 618: 'TONACATEPEQUE', 619: 'DELGADO', 701: 'CANDELARIA', 702: 'COJUTEPEQUE', 703: 'EL CARMEN', 704: 'EL ROSARIO', 705: 'MONTE SAN JUAN', 706: 'ORATORIO DE CONCEPCIÓN', 707: 'SAN BARTOLOME PERULAPÍA', 708: 'SAN CRISTOBAL', 709: 'SAN JOSÉ GUAYABAL', 710: 'SAN PEDRO PERULAPÁN', 711: 'SAN RAFAEL CEDROS', 712: 'SAN RAMÓN', 713: 'SANTA CRUZ ANALQUITO', 714: 'SANTA CRUZ MICHAPA', 715: 'SUCHITOTO', 716: 'TENANCINGO', 801: 'CUYULTITÁN', 802: 'EL ROSARIO', 803: 'JERUSALÉN', 804: 'MERCEDES LA CEIBA', 805: 'OLOCUILTA', 806: 'PARAISO DE OSORIO', 807: 'SAN ANTONIO MASAHUAT', 808: 'SAN EMIGDIO', 809: 'SAN FRANCISCO CHINAMECA', 810: 'SAN JUAN NONUALCO', 811: 'SAN JUAN TALPA', 812: 'SAN JUAN TEPEZONTES', 813: 'SAN LUIS TALPA', 814: 'SAN MIGUEL TEPEZONTES', 815: 'SAN PEDRO MASAHUAT', 816: 'SAN PEDRO NONUALCO', 817: 'SAN RAFAEL OBRAJUELO', 818: 'SANTA MARÍA OSTUMA', 819: 'SANTIAGO NONUALCO', 820: 'TAPALHUACA', 821: 'ZACATECOLUCA', 822: 'SAN LUIS LA HERRADURA', 901: 'CINQUERA', 902: 'GUACOTECTI', 903: 'ILOBASCO', 904: 'JUTIAPA', 905: 'SAN ISIDRO', 906: 'SENSUNTEPEQUE', 907: 'TEJUTEPEQUE', 908: 'VICTORIA', 909: 'VILLA DOLORES', 1001: 'APASTEPEQUE', 1002: 'GUADALUPE', 1003: 'SAN CAYETANO ISTEPEQUE', 1004: 'SANTA CLARA', 1005: 'SANTO DOMINGO', 1006: 'SAN ESTEBAN CATARINA', 1007: 'SAN ILDEFONSO', 1008: 'SAN LORENZO', 1009: 'SAN SEBASTIÁN', 1010: 'SAN VICENTE', 1011: 'TECOLUCA', 1012: 'TEPETITÁN', 1013: 'VERAPAZ', 1101: 'ALEGRÍA', 1102: 'BERLÍN', 1103: 'CALIFORNIA', 1104: 'CONCEPCIÓN BATRES', 1105: 'EL TRIUNFO', 1106: 'EREGUAYQUÍN', 1107: 'ESTANZUELAS', 1108: 'JIQUILISCO', 1109: 'JUCUAPA', 1110: 'JUCUARÁN', 1111: 'MERCEDES UMAÑA', 1112: 'NUEVA GRANADA', 1113: 'OZATLÁN', 1114: 'PUERTO EL TRIUNFO', 1115: 'SAN AGUSTÍN', 1116: 'SAN BUENA VENTURA', 1117: 'SAN DIONISIO', 1118: 'SANTA ELENA', 1119: 'SAN FRANCISCO JAVIER', 1120: 'SANTA MARÍA', 1121: 'SANTIAGO DE MARÍA', 1122: 'TECAPÁN', 1123: 'USULUTÁN', 1201: 'CAROLINA', 1202: 'CIUDAD BARRIOS', 1203: 'COMACARÁN', 1204: 'CHAPELTIQUE', 1205: 'CHINAMECA', 1206: 'CHIRILAGUA', 1207: 'EL TRÁNSITO', 1208: 'LOLOTIQUE', 1209: 'MONCAGUA', 1210: 'NUEVA GUADALUPE', 1211: 'NUEVO EDÉN DE SAN JUAN', 1212: 'QUELEPA', 1213: 'SAN ANTONIO', 1214: 'SAN GERARDO', 1215: 'SAN JORGE', 1216: 'SAN LUIS DE LA REINA', 1217: 'SAN MIGUEL', 1218: 'SAN RAFAEL ORIENTE', 1219: 'SESORI', 1220: 'ULUAZAPA', 1301: 'ARAMBALA', 1302: 'CACAOPERA', 1303: 'CORINTO', 1304: 'CHILANGA', 1305: 'DELICIAS DE CONCEPCIÓN', 1306: 'EL DIVISADERO', 1307: 'EL ROSARIO', 1308: 'GUALOCOCTI', 1309: 'GUATAJIAGA', 1310: 'JOATECA', 1311: 'JOCOAITIQUE', 1312: 'JOCORO', 1313: 'LOLOTIQUILLO', 1314: 'MEANGUERA', 1315: 'OSICALA', 1316: 'PERQUÍN', 1317: 'SAN CARLOS', 1318: 'SAN FERNANDO', 1319: 'SAN FRANCISCO GOTERA', 1320: 'SAN ISIDRO', 1321: 'SAN SIMÓN', 1322: 'SENSEMBRA', 1323: 'SOCIEDAD', 1324: 'TOROLA', 1325: 'YAMABAL', 1326: 'YOLOAIQUÍN', 1401: 'ANAMORÓS', 1402: 'BOLÍVAR', 1403: 'CONCEPCIÓN DE ORIENTE', 1404: 'CONCHAGUA', 1405: 'EL CARMEN', 1406: 'EL SAUCE', 1407: 'INTIPUCÁ', 1408: 'LA UNIÓN', 1409: 'LISLIQUE', 1410: 'MEANGUERA DEL GOLFO', 1411: 'NUEVA ESPARTA', 1412: 'PASAQUINA', 1413: 'POLORÓS', 1414: 'SAN ALEJO', 1415: 'SAN JOSÉ', 1416: 'SANTA ROSA DE LIMA', 1417: 'YAYANTIQUE', 1418: 'YUCUAIQUÍN'}
 
     #Devuelve diccionario con departamento y municipio
-    obtener_ubicacion: Callable[[str], dict] = lambda codigo: {
-        AtributosCentroEscolar.DEPARTAMENTO_CENTRO: UbicacionCentroEscolar.LISTA_DEPARTAMENTOS[int(codigo[:-2])],
-        AtributosCentroEscolar.MUNICIPIO_CENTRO: UbicacionCentroEscolar.LISTA_MUNICIPIOS[int(codigo[:])]
+    @classmethod
+    def obtener_ubicacion(cls, codigo:str) -> dict:
+        return {
+        cls.DEPARTAMENTO_CENTRO: cls.LISTA_DEPARTAMENTOS[int(codigo[:-2])],
+        cls.MUNICIPIO_CENTRO: cls.LISTA_MUNICIPIOS[int(codigo[:])]
         }
 
 
@@ -56,6 +57,7 @@ class ErroresPrevistos:
     FUERA_DE_RANGO = "El valor ingresado en el campo no está dentro del rango permitido"
     BUSQUEDA_SIN_RESULTADOS = "No se han encontrado resultados de búsqueda con la información proporcionada."
     OTRAS_CAUSAS = "Error en el servidor."
+    CARACTER_NO_VALIDO = "El nombre contiene un caracter no permitido"
 
     # Para listar detalles, donde aplique.
     detalles: Callable[[Exception], str] = lambda error:  ("\n  Posible(s) causa(s):\n * " + "\n * ".join(error.args)) if len(error.args) > 0 else ""
@@ -85,7 +87,7 @@ class EstilosImpresionCaracteres:
     mix_estilos: Callable[[tuple], str] = lambda *estilos: "".join([EstilosImpresionCaracteres.__dict__[i] for i in estilos])
 
 
-class __CodigosColores:
+class CodigosColores:
     '''
     Clase que heredan  las regiones y caracteres a colorear.
     '''
@@ -106,16 +108,15 @@ class __CodigosColores:
     #CARACTER_CLARO = 9
     #FONDO_CLARO = 10
 
-
-def __obtener_codigos(texto_o_fondo: int) -> tuple:
-    lista_codigos = []
-    for (k, v) in OrderedDict(__CodigosColores.__dict__).items():
-        if len(k)==1:
-            codigo = texto_o_fondo * 10 + v
-            codigo = f'\033[{codigo}m'
-            lista_codigos.append(codigo)
-    print(*lista_codigos)
-    return lista_codigos
+    @classmethod
+    def obtener_codigos(cls,texto_o_fondo: int) -> tuple:
+        lista_codigos = []
+        for (k, v) in OrderedDict(CodigosColores.__dict__).items():
+            if len(k)==1:
+                codigo = texto_o_fondo * 10 + v
+                codigo = f'\033[{codigo}m'
+                lista_codigos.append(codigo)
+        return lista_codigos
 
 class ColoresImpresionDisponibles:
     '''
@@ -130,62 +131,70 @@ class ColoresImpresionDisponibles:
     '''
 
     
-    class CARACTER(__CodigosColores):
-        D, R, G, Y, B, M, G, W = __obtener_codigos(__CodigosColores.CARACTER)
-    class F0ND0:
-        D, R, G, Y, B, M, G, W = __obtener_codigos(__CodigosColores.FONDO)
+    class CARACTER(CodigosColores):
+        D, R, G, Y, B, M, G, W = CodigosColores.obtener_codigos(CodigosColores.CARACTER)
+    class F0ND0(CodigosColores):
+        D, R, G, Y, B, M, G, W = CodigosColores.obtener_codigos(CodigosColores.FONDO)
 
 
+class AtributosRespuestasModeloDB(Base_):
+    ESTADO: str = "ESTADO"
+    TIPO: str  = "TIPO"
+    CONTENIDO: Union[str, list, Iterator] = "CONTENIDO"
 
-'''
-Variables globales creadas para manejar las respuestas a las peticiones y reducir
-errores al escribirlas en cada respuesta, aprovechando el autocompletado
-'''
-# Estados  de las respuestas a la interfaz
-estado = "estado"
-exitoso = "exitoso"
-fallido = "fallido"
-# Tipos de respuestas a la interfaz
-tipo_respuesta = "tipo_respuesta"
-mensaje = "mensaje"
-lista = "lista"
-generador = "generador"
-# Carga últil para las respuestas
-respuesta = "respuesta"
+class EstadosRespuesta:
+    EXITOSO: str = "exitoso"
+    FALLIDO: str = "fallido"
 
-estado_conexion_exitoso = {
-    estado:exitoso,
-    tipo_respuesta:mensaje,
-    respuesta: "Conexión con el servidor exitosa."
-}
-devolucion_informacion_exitosa = {
-    estado: exitoso,
-    tipo_respuesta: lista
-}
-devolucion_generador_exitosa = {
-    estado:exitoso,
-    tipo_respuesta:generador
-}
-    # Fracasos
-resultados_fallidos = {
-    estado:fallido,
-    tipo_respuesta: mensaje
-}
+class TiposRespuesta:
+    MENSAJE: str = "mensaje"
+    LISTA: str = "lista"
+    GENERADOR: Iterator = None
+
+class TiposContenido:
+    MENSAJE: str = "MENSAJE"
+    LISTA: list = "LISTA"
+    GENERADOR: Iterator = "GENERADOR"
+
+class Respuestas(AtributosRespuestasModeloDB, EstadosRespuesta, TiposRespuesta):
+    '''
+    Manejo de respuestas a las peticiones 
+    '''
+    @classmethod
+    def __preparar_respuesta(cls, estado: str, tipo: str, contenido: Union[str, list, Iterator]):
+        return {
+            cls.ESTADO: estado,
+            cls.TIPO: tipo,
+            cls.CONTENIDO: contenido
+        }        
 
 
+    @classmethod
+    def estado_conexion_exitoso(cls):
+        mensaje = "Conexión con el servidor exitosa."
+        return cls.__preparar_respuesta(cls.EXITOSO, cls.MENSAJE, mensaje)
+            
+    @classmethod
+    def devolucion_informacion_exitosa(cls, lista: list):
+        return cls.__preparar_respuesta(cls.EXITOSO, cls.LISTA, lista)
 
+    @classmethod
+    def devolucion_generador_exitosa(cls, generador: Iterator):
+        return cls.__preparar_respuesta(cls.EXITOSO, cls.GENERADOR, generador)
 
-if __name__=="__main__":
-    #Pruebas
-    def tests():
+    @classmethod
+    def resultados_fallidos(cls, mensaje: str, detalles_error: Exception):
+        return cls.__preparar_respuesta(cls.FALLIDO, cls.MENSAJE, mensaje + ErroresPrevistos.detalles(detalles_error))
 
-        print(AtributosCentroEscolar.obtener_como_objeto())
+class Test:
+    def test_AtributosCentroEscolar():
+        print(AtributosCentroEscolar.obtener_como_diccionario)
 
-
+    def test_UbicacionCentroEscolar():
         print(UbicacionCentroEscolar.LISTA_DEPARTAMENTOS)
         print(UbicacionCentroEscolar.obtener_ubicacion('1123'))
 
-
+    def test_ErroresPrevistos():
         print(ErroresPrevistos.FIN_ITERACIONES)
         class argumento_error0:
             args = ()
@@ -198,12 +207,12 @@ if __name__=="__main__":
                     args = tuple(["Blah" for j in range(i)])
             print(ErroresPrevistos.detalles(argumento_error))
 
-
+    def test_EstilosImpresionCaracteres():
         print(EstilosImpresionCaracteres.NEGRITA + EstilosImpresionCaracteres.CURSIVA + EstilosImpresionCaracteres.TACHADO + EstilosImpresionCaracteres.INVERTIR_FONDO_CARACTER + 'Mezcado manual')
         print(EstilosImpresionCaracteres.NORMAL + "Sin formato")
         print(EstilosImpresionCaracteres.mix_estilos("NEGRITA", "CURSIVA", "INVERTIR"))
 
-
+    def test_ColoresImpresionDisponibles():
         print(EstilosImpresionCaracteres.NORMAL + "Hola", sep="\n")
         print(ColoresImpresionDisponibles.CARACTER.R + "Hola hola", sep="\n")
         print(ColoresImpresionDisponibles.F0ND0.Y + "Hola hola", sep="\n")
@@ -212,3 +221,22 @@ if __name__=="__main__":
         print(EstilosImpresionCaracteres.NORMAL + "Hola", sep="\n")
 
 
+
+
+if __name__=="__main__":
+    Respuestas.estado_conexion_exitoso(12)
+
+
+""" 
+class Class: # Solo definir para el tipo de entrada
+    pass
+def obtener_diccionario_atributos(clase_o_funcion: Callable[[Union[Callable, Class]], dict]) -> dict:
+    '''
+    Para recuperar plantillas de diccionarios en algunas clases o funciones
+    '''
+    diccionario_orden_fijo: OrderedDict = OrderedDict(clase_o_funcion.__dict__)
+    lista_tuplas_diccionario: tuple = diccionario_orden_fijo.items()
+    filtro_atributos_privados: Callable[[str], bool] = lambda clave, valor: "__" not in clave
+    resultado_filtro: filter = filter(filtro_atributos_privados,lista_tuplas_diccionario)
+    return dict(resultado_filtro)
+ """
--- modelodb.py
+++ modelodb.py
@@ -3,12 +3,12 @@ from typing import ( # Mejora las anotaciones en el código
     Union, 
     Optional, 
     Callable, 
-    Generator,
-    Exception
-    ) 
+    Generator
+    )
 from configuracion import( # Variables globales
     ErroresPrevistos as Error,
-    AtributosCentroEscolar as CE
+    AtributosCentroEscolar as CE,
+    Respuestas as Resp
     )
 from functools import wraps # Preserva la información de funciones, como el nombre, lo uso en gestión de errores
 from pymongo import MongoClient
@@ -21,64 +21,39 @@ from pymongo.errors import (
     )
 
 
-def with_error_handler(funcion: Callable) -> Callable:
+def con_gestion_de_errores(funcion: Callable) -> Callable:
 
     """ Gestion de los errores en la ejecución del código """
 
     @wraps(funcion)
     def decorador(*args: Union[int, str, dict], **kwargs: Optional[int]) -> dict:
         try:
-            # Validación de errores específicos del método
-            
-            if funcion.__name__ == "obtener_por_codigo":
-                assert type(args[1])==int, Error.no_numero
-                assert args[1] > 9999, Error.numero_invalido
-            #if funcion.__name__ == "":
-            #    pass
             resultado = funcion(*args, *kwargs)
-            assert resultado[respuesta] != [], Error.BUSQUEDA_SIN_RESULTADOS
-            assert resultado[respuesta] != [None], Error.BUSQUEDA_SIN_RESULTADOS
             return resultado
         except AssertionError as error:
-            return {
-                **resultados_fallidos,
-                respuesta: error.args[0]
-            }
+            return Resp.resultados_fallidos("Error en los datos.", error)
+
         except (AutoReconnect, ConnectionFailure) as error:
-            return {
-                **resultados_fallidos, 
-                respuesta: Error.CONEXION_FALLIDA + Error.detalles(error)
-            }
+            return Resp.resultados_fallidos(Error.CONEXION_FALLIDA, error)
+
         except (ExecutionTimeout, ServerSelectionTimeoutError) as error:
-            return {
-                **resultados_fallidos, 
-                respuesta: Error.TIEMPO_EXCEDIDO + Error.detalles(error)
-            }
+            return Resp.resultados_fallidos(Error.TIEMPO_EXCEDIDO, error)
+
         except OperationFailure as error:
-            return {
-                **resultados_fallidos,
-                respuesta: Error.PETICIONES_INCORRECTAS + Error.detalles(error)
-            }
+            return Resp.resultados_fallidos(Error.PETICIONES_INCORRECTAS, error)
+
         except StopIteration as error: # FALTA MANEJAR LOS GENERADORES
-            return {
-                **resultados_fallidos,
-                respuesta: Error.FIN_ITERACIONES + Error.detalles(error)
-            }
+            return Resp.resultados_fallidos(Error.FIN_ITERACIONES, error)
+
         except KeyError as error:
-            return {
-                **resultados_fallidos,
-                respuesta: Error.FUERA_DE_RANGO + Error.detalles(error)
-            }
+            return Resp.resultados_fallidos(Error.FUERA_DE_RANGO, error)
+
         except ValueError as error:
-            return {
-                **resultados_fallidos,
-                respuesta: Error.NO_ES_NUMERO + Error.detalles(error)
-            }
+            return Resp.resultados_fallidos(Error.NO_ES_NUMERO, error)
+
         except Exception as error:
-            return {
-                **resultados_fallidos,
-                respuesta: Error.OTRAS_CAUSAS  + Error.detalles(error)
-            }
+            return Resp.resultados_fallidos(Error.OTRAS_CAUSAS, error)
+
     return decorador
 
 
@@ -91,30 +66,20 @@ class CentroEducativo():
     def __init__(self) -> None:
         self.estado_conexion = self.__conexion_servidor_mongodb()
 
-    @with_error_handler
+    @con_gestion_de_errores #REVISADO
     def __conexion_servidor_mongodb(self) -> dict:
         global __coleccion_ce # Se utiliza global porque se modificará la variable.
         cliente = MongoClient(open(".serverkey").read())
         db = cliente["Evaluacion01"]
         __coleccion_ce = db["CE"]
-        return estado_conexion_exitoso
-
-    #@with_error_handler.  FALTA REFACTORIZAR PARA QUE EL DECORADOR SOLO SE NECESITE AQUÍ Y EN OTRO PAR MÁS DE FUNCIONES
+        return Resp.estado_conexion_exitoso()
+    #REVISADO
     def __ejecutar_busqueda(self, *args: Union[dict, Optional[int]]) -> dict:
         cursor = __coleccion_ce.find(args[0], {"_id":0})
-        if args[0] == {}:
-            return {
-                **devolucion_generador_exitosa,
-                respuesta: cursor.batch_size(args[1])
-            }
         resultados_busqueda = list(cursor)
-        return {
-            **devolucion_informacion_exitosa,
-            respuesta: resultados_busqueda
-        }
+        return Resp.devolucion_informacion_exitosa(resultados_busqueda)
 
-    #@with_error_handler
-    @staticmethod
+    @staticmethod #REVISADO
     def funcion_generadora(cursor: Iterator,  cantidad_a_obtener: int, cantidad_por_pagina: int) -> Generator:
         if not cantidad_a_obtener:
             cantidad_a_obtener = __coleccion_ce.count()
@@ -126,41 +91,60 @@ class CentroEducativo():
             cursor.close()
 
 
-    @with_error_handler
+    @con_gestion_de_errores #REVISADO
     def agregar_centro_educativo(self, objeto_centro: dict) -> dict:
+        revision_coincidencias = __coleccion_ce.find_one(objeto_centro)
+        assert revision_coincidencias == None, Error.PETICIONES_INCORRECTAS \
+            + "\nSe encontró un documento con este nombre"
         resultado = __coleccion_ce.insert_one(objeto_centro)
-        confirmacion = __coleccion_ce.find({"_id": resultado.inserted_id})
-        return confirmacion
+        confirmacion = __coleccion_ce.find({"_id": resultado.inserted_id}, {"_id": 0})
+        confirmacion = list(confirmacion)
+        return Resp.devolucion_informacion_exitosa(confirmacion)
 
-    @with_error_handler
+    @con_gestion_de_errores #REVISADO
     def obtener_por_codigo(self, codigo_infraestructura: int) -> dict:
-        centro_educativo = self.__ejecutar_busqueda({"CODIGO": codigo_infraestructura})
+        codigo_infraestructura = int(codigo_infraestructura)
+        assert codigo_infraestructura > 9999, Error.FUERA_DE_RANGO
+        centro_educativo = self.__ejecutar_busqueda({CE.CODIGO: int(codigo_infraestructura)})
         return centro_educativo
 
-    @with_error_handler
+    @con_gestion_de_errores #REVISADO
     def obtener_por_nombre(self, nombre_centro_educativo: str) -> dict:
-        centros_educativos = self.__ejecutar_busqueda({"NOMBRE": nombre_centro_educativo})
+        assert "{" not in nombre_centro_educativo, Error.CARACTER_NO_VALIDO
+        assert "$" not in nombre_centro_educativo, Error.CARACTER_NO_VALIDO
+        diccionario_busqueda = {
+            "$text": {
+                "$search": nombre_centro_educativo,
+                "$language": "spanish",
+                "$caseSensitive": False,
+                "$diacriticSensitive": False
+                }
+            }
+        centros_educativos = self.__ejecutar_busqueda({"NOMBRE": diccionario_busqueda})
         return centros_educativos
 
-    @with_error_handler
+    @con_gestion_de_errores #REVISADO
     def obtener_todos(self, cantidad_a_obtener: int, cantidad_por_pagina: int = 10) -> dict:
-        resultado_consulta = self.__ejecutar_busqueda({}, cantidad_por_pagina)
-        cursor = resultado_consulta[respuesta]
-        resultado_consulta[respuesta] = CentroEducativo.funcion_generadora(cursor, cantidad_a_obtener, cantidad_por_pagina)
-        return resultado_consulta
+        cantidad_a_obtener = int(cantidad_a_obtener)
+        cantidad_por_pagina = int(cantidad_por_pagina)
+        cursor = __coleccion_ce.find({}, {"_id":0}).batch_size(cantidad_por_pagina)
+        funcion_generadora: Iterator = CentroEducativo.funcion_generadora(cursor, cantidad_a_obtener, cantidad_por_pagina)
+        return Resp.devolucion_generador_exitosa(funcion_generadora)
 
-    @with_error_handler
+    @con_gestion_de_errores #REVISADO
     def actualizar_centro_educativo(self, modificacion_centro_educativo: dict) -> dict:
         resultado_busqueda = self.obtener_por_codigo({CE.CODIGO: modificacion_centro_educativo[CE.CODIGO]})
-        centro_educativo_obtenido = resultado_busqueda[respuesta][0]
+        centro_educativo_obtenido = resultado_busqueda[Resp.CONTENIDO][0]
         resultado_actualizacion = __coleccion_ce.find_one_and_update(centro_educativo_obtenido, {"$set": modificacion_centro_educativo})
         del resultado_actualizacion["_id"]
         return [resultado_actualizacion]
 
-    @with_error_handler
+    @con_gestion_de_errores #REVISADO
     def eliminar_centro_educativo(self, codigo_infraestructura: int) -> dict:
-        resultado_eliminacion = __coleccion_ce.find_one_and_delete({CE.CODIGO: codigo_infraestructura})
-        return [resultado_eliminacion]
+        codigo_infraestructura = int(codigo_infraestructura)
+        assert codigo_infraestructura > 10000,  Error.FUERA_DE_RANGO
+        resultado_eliminacion = __coleccion_ce.find_one_and_delete({CE.CODIGO: codigo_infraestructura}, {"_id":0})
+        return Resp.devolucion_informacion_exitosa([resultado_eliminacion])
 
 
 
@@ -175,15 +159,10 @@ if __name__ == "__main__":
         CE.DEPARTAMENTO_CENTRO: "SAN MIGUEL",
         CE.MUNICIPIO_CENTRO: "SAN MIGUEL"
     }
-    prueba_insert_1 = {
-        CE.CODIGO: 13095,
-        CE.NOMBRE: "CENTRO ESCOLAR NIÑO JESÚS DE PRAGA",
-        CE.DEPARTAMENTO_CENTRO: "SAN MIGUEL",
-        CE.MUNICIPIO_CENTRO: "SAN MIGUEL"
-    }
 
-
-    #ce.agregar_centro_educativo(prueba_insert_1)
+    print(ce.agregar_centro_educativo(prueba_insert_1))
+    print(ce.obtener_por_nombre("NIÑO JESUS DE PRAGA"))
+    #print(ce.eliminar_centro_educativo("13095"))
     #ce.agregar_centro_educativo(prueba_insert_2)
 
     """
@@ -198,7 +177,7 @@ if __name__ == "__main__":
 
     """  
     # Prueba generador
-    generador = ce.obtener_todos(29, 10)[respuesta]
+    generador = ce.obtener_todos(29, 10)[Resp.CONTENIDO]
     
     # Versión con bucle for.
     for i in generador:
